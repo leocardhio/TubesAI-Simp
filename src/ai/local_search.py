76d8014bfc845a70d74b5e16edc6b4f2efb28672
L. James <leonardusjamesw@gmail.com>
2021-10-01 14:00:09 +0700

    masukin implementasi local search complete dan minimax tanpa depth dari tucil1

--- src/ai/local_search.py
+++ src/ai/local_search.py
@@ -13,7 +13,67 @@ class LocalSearch:
 
     def find(self, state: State, n_player: int, thinking_time: float) -> Tuple[str, str]:
         self.thinking_time = time() + thinking_time
+        quota = state.players[n_player].quota
+        board = state.board
+        max_value_direction = [0, (0,0), ShapeConstant.BLANK]
+        optimal_col = 0
+        optimal_shape = ShapeConstant.BLANK
 
-        best_movement = (random.randint(0, state.board.col), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE])) #minimax algorithm
+        for col in range(0, board.col):
+            for row in range(state.board.row - 1, -1, -1):
+                if state.board[row, col].shape == ShapeConstant.BLANK:
+                    temp_value_direction = self.positionMaxValue(state, n_player, row, col)
+                    if max_value_direction[0] < temp_value_direction[0]:
+                        max_value_direction = temp_value_direction
+                        optimal_col = col
+                    if max_value_direction[0] == temp_value_direction[0] and max_value_direction[2] == ShapeConstant.BLANK and temp_value_direction[2] != ShapeConstant.BLANK:
+                        max_value_direction = temp_value_direction
+                        optimal_col = col
+                    break
+        
+        if max_value_direction[2] != ShapeConstant.BLANK:
+            optimal_shape = max_value_direction[2]
+        else:
+            optimal_shape = random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE]);
 
-        return None
+        best_movement = (optimal_col, optimal_shape)
+
+        return best_movement
+    
+    def positionMaxValue(self, state: State, n_player: int, row: int, col: int):
+        streak_way = [(-1, 0), (1, 0), (0, -1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
+        shape = state.players[n_player].shape
+        color = state.players[n_player].color
+        max_value_direction = [0, (0,0), ShapeConstant.BLANK] #[value, streak_way, shape]
+
+        for row_ax, col_ax in streak_way:
+            row_ = row + row_ax
+            col_ = col + col_ax
+            shapevalue = 0
+            colorvalue = 0
+            shape_streak = True
+            color_streak = True
+
+            for _ in range(GameConstant.N_COMPONENT_STREAK - 1):
+                if is_out(state.board, row_, col_):
+                    break
+
+                if shape_streak and shape == state.board[row_, col_].shape:
+                    shapevalue += 1
+                else:
+                    shape_streak = False
+                
+                if color_streak and color == state.board[row_, col_].color:
+                    colorvalue += 1
+                else:
+                    color_streak = False
+
+                row_ = row_ + row_ax
+                col_ = col_ + col_ax
+            
+            if shapevalue > max_value_direction[0]:
+                max_value_direction = [shapevalue, (row_ax, col_ax), shape]
+            if colorvalue > max_value_direction[0]:
+                max_value_direction = [colorvalue, (row_ax, col_ax), ShapeConstant.BLANK]
+        
+        return max_value_direction
--- src/ai/minimax.py
+++ src/ai/minimax.py
@@ -14,6 +14,76 @@ class Minimax:
     def find(self, state: State, n_player: int, thinking_time: float) -> Tuple[str, str]:
         self.thinking_time = time() + thinking_time
 
-        best_movement = (random.randint(0, state.board.col), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE])) #minimax algorithm
+        minimax = self.minimax(state, n_player, -10, 10, True)
+
+        best_movement = (minimax[1], minimax[2]) #minimax algorithm
 
         return best_movement
+
+    def minimax(self, state: State, n_player: int, alpha: int, beta: int, maxTurn: bool):
+        node_value = self.node_value(state, n_player)
+        if node_value != None:
+            print(f"Terminal value: {node_value}")
+            return [node_value, -1, ShapeConstant.BLANK]
+        
+        doubleBreak = False
+        if maxTurn:
+            maxEval = -10 #-10 maksudnya sebagai -infinit
+            maxTurn = [-1, ShapeConstant.BLANK] #[col, shape]
+            for col in range(state.board.col):
+                for shape in [ShapeConstant.CROSS, ShapeConstant.CIRCLE]:
+                    new_state = deepcopy(state)
+                    row = place(new_state, n_player, shape, col)
+                    if (row == - 1):
+                        break
+                    print(f"MaxTurn: {col}, {shape}")
+                    Eval = self.minimax(new_state, n_player, alpha, beta, False)[0]
+                    if Eval > maxEval:
+                        maxEval = Eval
+                        maxTurn = [col, shape]
+                        alpha = Eval
+                    if Eval == 1:
+                        return [maxEval, maxTurn[0], maxTurn[1]]
+                    if beta <= alpha:
+                        doubleBreak = True
+                        break
+                if doubleBreak:
+                    break
+            return [maxEval, maxTurn[0], maxTurn[1]]
+
+        else:
+            minEval = 10 #10 maksudnya sebagai infinit
+            minTurn = [-1, ShapeConstant.BLANK] #[col, shape]
+            for col in range(state.board.col):
+                for shape in [ShapeConstant.CROSS, ShapeConstant.CIRCLE]:
+                    new_state = deepcopy(state)
+                    row = place(new_state, (n_player+1)%2, shape, col)
+                    if (row == - 1):
+                        break
+                    print(f"MinTurn: {col}, {shape}")
+                    Eval = self.minimax(new_state, n_player, alpha, beta, True)[0]
+                    if Eval < minEval:
+                        minEval = Eval
+                        minTurn = [col, shape]
+                        beta = Eval
+                    if Eval == -1:
+                        return [minEval, minTurn[0], minTurn[1]]
+                    if beta <= alpha:
+                        doubleBreak = True
+                        break
+                if doubleBreak:
+                    break
+            return [minEval, minTurn[0], minTurn[1]]
+
+
+    def node_value(self, state: State, n_player: int):
+        winner = is_win(state.board)
+        if winner:
+            if winner[0] == state.players[n_player].shape and winner[1] == state.players[n_player].color:
+                return 1
+            else:
+                return -1
+        elif is_full(state.board):
+            return 0
+        else:
+            return None
